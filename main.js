02f1d2ec249b2bcc511c9c1b72761a121fd39ddc
Yonatanitzhak7 <Yonatan1171@gmail.com>
2022-05-21 21:59:26 +0300

    commit

---
+++ main.js
@@ -0,0 +1,243 @@
+// var user = prompt("Are u regiseter user?");
+// var age = prompt("Are u above 18?")
+// age = 10
+// if(age>18){
+//   document.write("Hello")
+// }
+// else{
+//   document.write("godbye")
+// }
+
+// var name = prompt("First name?");
+// document.write(name);
+
+// var age = +prompt ("What is your age?");
+// document.write(age);
+
+// var yon = 0
+// while(yon < 10){
+//   console.log("Yonatan")
+//   yon++;
+// }
+
+// var num = 0
+// while(num<100){
+//   console.log(num)
+//   num++;
+// }
+
+// console.log("ISRAEL");
+
+// console.log("Yonatan");
+
+// console.log(77);
+
+// var num = 0
+// while(num<5){
+//   console.log("77");
+//   num++;
+// }
+
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = 3;
+// var num4 = 4;
+// avg = num1+num2+num3+num4/4;
+// sum = num1+num2+num3+num4;
+// console.log(avg);
+// console.log(sum);
+
+// var width = 20;
+// var height = 10;
+// sum = width*height;
+// console.log(sum);
+
+// var num1 = 1;
+// var num2 = 2;
+// var num3 = 0;
+// num3 = num1
+// num1 = num2
+// num2 = num3
+// console.log(num1);
+// console.log(num2);
+
+// var num = 0
+// while(num<5){
+//   +prompt("enter a number");
+//   num++;
+// }
+
+// console.log(+prompt("enter number"));
+
+// var a1 = "a"
+// var a2 = "1"
+// console.log(a1,a2);
+
+// var a = true
+// console.log(a);
+
+// var mul = 1;
+// for (var i = 0; i < 3; i++) {
+//   var num1 = +prompt("Enter a number");
+//   mul = num1 * mul;
+// }
+// console.log(mul);
+
+// function printYonatan () {
+//     for(var i = 0; i<3; i++){
+//         document.write("Yonatan")
+//     }
+
+// }
+// printYonatan ()
+
+// var first = 7;
+// var second = 2;
+// var result = first%second;
+// console.log(result)
+
+// var num = +prompt("number")
+// num++;
+// console.log(num)
+
+// var num = +prompt("number")
+// num *= 3;
+// console.log(num);
+
+// var radius = +prompt("radius")
+// var perimeter = 2*radius*3.14
+// var space = 3.14*radius*radius
+// console.log(perimeter,space);
+
+// var name1 = prompt("name")
+// var age1 = +prompt("age")
+// var name2 = prompt ("name")
+// var age2 = +prompt("age")
+// var name3 = prompt ("name")
+// var age3 = +prompt("age")
+// console.log(name1,name2,name3)
+// var avg = (age1+age2+age3)/3
+// console.log(avg);
+
+// var sum =0;
+// for(var i =0; i<3; i++){
+//     var name = prompt("name")
+//     console.log(name);
+//     var age = +prompt("age")
+//     sum += age
+// }
+// var avg = sum/3;
+// console.log(avg);
+
+
+// var sum =0;
+// for(var i =0; i<3; i++){
+//     console.log(prompt("name"));
+//     sum += +prompt("age")
+// }
+// console.log(sum/3);
+
+// console.log(+prompt("number")*3);
+
+// var b=4;
+// var d = 4 + ++b;
+// b = b+1;
+// var d = 4+b;
+
+// var b = 4;
+// var d = 4 + b--;
+// var d = 4+b;
+// b = b-1
+// ----------------------------------------------------------
+// function getCircleAreaPerimeter(radius){
+//     var ca = 2*3.14*radius;
+//     var cp = 3.14**radius;
+//     console.log(ca,cp);
+// }
+// console.log(getCircleAreaPerimeter(3));
+
+// function getPlayerInfo(){
+//     for(var i=0;i<3;i++){
+//         var playername = prompt("Enter Name")
+//         var playerage = +prompt("Enter Age")
+//         var sum = 0;
+//         sum+=playerage
+//         console.log(playername);
+//     }
+//    var avg =sum/3
+//    console.log(avg);
+// }
+// getPlayerInfo()
+
+// function oneToHundred(){
+//     for(var i=1;i<101;i++){
+//         console.log(i);
+//     }
+// }
+// oneToHundred()
+
+// function getSumAndAvg(){
+//     var sum = 0;
+//     for(var i=0;i<5;i++){
+//         var num = +prompt("Enter Number");
+//         sum+=num;
+//     }
+//     var avg = sum/5;
+//     console.log(avg,sum);
+
+// }
+// getSumAndAvg()
+
+// function evenNumbers(){
+//     for(var i=0;i<51;i++){
+//         if(i%2==0){
+//             console.log(i);
+//         }
+//     }
+// }
+// evenNumbers()
+
+// function divisionByThree() {
+//   for (var i = 0; i < 50; i++) {
+//     if (i%3==1)  {
+//         console.log(i);
+//     }
+//   }
+// }
+// divisionByThree()
+
+// function getAssembly() {
+//   var num = +prompt("Enter Number");
+//   var sum = 1;
+//   for (var i = num; i > 0; i--) {
+//     sum *= num;
+//     num--;
+//   }
+//   console.log(sum);
+// }
+// getAssembly();
+
+// function minAndMax(){
+//     var sum=0;
+//     var avg =0;
+//     for(var i=0;i<5;i++){
+//         num = +prompt("Enter Number")
+//         sum+=num;
+//         avg=sum/5
+//     }
+//     console.log(sum,avg);
+// }
+// minAndMax()
+
+// function getMaxAndMin(){
+//     max=0;
+//     min=0;
+//     for(var i=0;i<0;i++){
+//         if(i%2==0){
+//             num = +prompt("Enter Number")
+             
+//         }
+//     }
+// }
+// ---------------------------------------------------
+
